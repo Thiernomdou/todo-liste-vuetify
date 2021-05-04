3958639bbbc8c6e38f5ad9f1c0827080985015a6
DIALLO Thierno Mamadou <thiernomdou63@gmail.com>
2021-05-05 01:50:02 +0200

    navigation

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>A Propos de Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, 
+      adipisicing elit. Maxime, reiciendis eaque quia 
+      cupiditate repellat obcaecati assumenda temporibus 
+      recusandae exercitationem alias iste placeat iure. 
+      Nobis dolorum ipsum asperiores delectus, assumenda impedit.
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur, 
+      adipisicing elit. Maxime, reiciendis eaque quia 
+      cupiditate repellat obcaecati assumenda temporibus 
+      recusandae exercitationem alias iste placeat iure. 
+      Nobis dolorum ipsum asperiores delectus, assumenda impedit.
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur, 
+      adipisicing elit. Maxime, reiciendis eaque quia 
+      cupiditate repellat obcaecati assumenda temporibus 
+      recusandae exercitationem alias iste placeat iure. 
+      Nobis dolorum ipsum asperiores delectus, assumenda impedit.
+    </p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home pa-6">
+    <h1>Votre Todo List</h1>
+  </div>
+</template>
+
+<script>
+  
+  export default {
+    name: 'Todo',
+
+    components: {
+
+    },
+  }
+</script>
