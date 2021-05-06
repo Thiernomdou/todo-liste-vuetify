c24d4f0f73d0e9a1d48f300f0539e21737549d4b
DIALLO Thierno Mamadou <thiernomdou63@gmail.com>
2021-05-06 23:16:51 +0200

    delete

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="home">
     <v-list class="pt-0" flat>
-      <div v-for="(langage, index) in langages" :key="index">
+      <div v-for="langage in langages" :key="langage.id">
       
-        <v-list-item @click="finiLangage(langage.index)">
+        <v-list-item @click="finiLangage(langage.id)" :class="{'blue lighten-5' :langage.fini}">
+          
           <template v-slot:default>
             <v-list-item-action>
               <v-checkbox
@@ -13,9 +14,20 @@
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>{{ langage.title }}</v-list-item-title>
+              <v-list-item-title 
+                :class="{'text-decoration-line-through' :langage.fini}">
+                {{ langage.title }}
+              </v-list-item-title>
             </v-list-item-content>
+
+          <v-list-item-action>
+            <v-btn @click.stop="deleteLangage(langage.id)" icon>
+              <v-icon color="primary lighten-1">mdi-delete</v-icon>
+            </v-btn>
+          </v-list-item-action>
+
           </template>
+
         </v-list-item>
       
         <v-divider></v-divider>
@@ -41,8 +53,11 @@
     },
     methods: {
       finiLangage(id) {
-        let langage = this.langages.filter(langage => langage.id === index)[0];
-        langage.fini = !langage.fini;
+        let langage = this.langages.filter(langage => langage.id === id)[0];
+        langage.fini =!langage.fini;
+      },
+      deleteLangage(id) {
+        this.langages = this.langages.filter(langage => langage.id !== id);
       }
     },
     components: {
